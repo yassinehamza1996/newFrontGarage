5f568455539d7d7c727c5b449a010c2c796c4255
yassinehamza <yassinehamza1996@gmail.com>
2023-06-16 12:16:47 +0100

    changes

--- gestionGarage/src/app/app.component.ts
+++ gestionGarage/src/app/app.component.ts
@@ -1,58 +1,110 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { City } from './city.model';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Car } from './models/cars.model';
+import { CarBrandsList } from './models/carsbrand.model';
+import { CarService } from './services/carBrandsService.service';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnChanges {
-
-  ngOnChanges(changes: SimpleChanges): void {
-    throw new Error('Method not implemented.');
-  }
-
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
-  cities: City[];
-  selectedCity: City;
+  cities: Car[];
+  motosList: Car[];
+  carsList: Car[];
+  carsBrands : CarBrandsList[];
+  selectedcarListModel: Car;
   name = 'Angular';
   isCollapsed = true;
   title = 'gestionGarage';
-  carBtn: boolean = false;
-  motoBtn: boolean = false;
-  isMoto : boolean = false;
-  isCar : boolean = false;
+  cm: string;
+  isMoto: boolean = false;
+  isCar: boolean = false;
+  displayAddCar: boolean = false;
+  form: FormGroup;
 
-  constructor() {
-    this.cities = [
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
-      { name: 'London', code: 'LDN' },
-      { name: 'Istanbul', code: 'IST' },
-      { name: 'Paris', code: 'PRS' },
-    ];
+  constructor(private carBrandService : CarService) {
     
-    this.cities = [
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
-      { name: 'London', code: 'LDN' },
-      { name: 'Istanbul', code: 'IST' },
-      { name: 'Paris', code: 'PRS' },
-    ];
+    this.motosList  = [
+      { "id":1,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "HARLEY DAVIDSON", "type": "Moto" , "description" : "MOTO HARLEY DAVIDSON 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":2,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "HONDA", "type": "Moto","description" : "HONDA 140 , #000000 ,Tunis 6590 , 140 Tunis 6590" },
+      { "id":3,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Audi", "type": "Moto" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":4,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Toyota", "type": "Moto" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":5,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "KTM", "type": "Moto" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":6,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Ford", "type": "Moto" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":7,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "KTM", "type": "Moto" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":8,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Nissan", "type": "Moto" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":9,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Volkswagen", "type": "Moto" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":10,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "TVS", "type": "Moto" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"}
+    ]
+
+    this.carsList = [
+      { "id":1,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "BMW", "type": "Voiture" , "description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":2,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Mercedes-Benz", "type": "Voiture","description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590" },
+      { "id":3,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Audi", "type": "Voiture" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":4,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Toyota", "type": "Voiture" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":5,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Honda", "type": "Voiture" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":6,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Ford", "type": "Voiture" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":7,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Chevrolet", "type": "Voiture" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":8,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Nissan", "type": "Voiture" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":9,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Volkswagen", "type": "Voiture" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"},
+      { "id":10,"annee": "2011", "immatriculation": "140 Tunis 6590", "couleur": "#000000", "marque": "Tesla", "type": "Voiture" ,"description" : "BMW 140 , #000000 ,Tunis 6590 , 140 Tunis 6590"}
+    ]
   }
 
-  carClicked() {
-    if (this.carBtn != false) {
-      this.motoBtn = false;
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  ngOnInit(): void {
+    this.carBrandService.fillCarBrandsList().subscribe(
+      (brands: CarBrandsList[]) => {
+        this.carsBrands = brands;
+        console.log(this.carsBrands); 
+      },
+      (error) => {
+        console.log('Error fetching car brands:', error);
+      }
+    );
+    this.form = this.createCarFormGroup()
+
+    
+  }
+  createCarFormGroup() : FormGroup{
+    return new FormGroup({
+      annee: new FormControl('', Validators.required),
+      immatriculation: new FormControl('', Validators.required),
+      couleur: new FormControl('#000000', Validators.required),
+      marque: new FormControl('', Validators.required),
+      type: new FormControl('Voiture', Validators.required),
+    });
+  }
+
+  radioClicked() {
+    if (this.cm == 'voiture') {
       this.isCar = true;
+      this.isMoto = false;
+    } else {
+      this.isMoto = true;
+      this.isCar = false;
     }
   }
-  motoClicked() {
-    if (this.motoBtn != false) {
-      this.carBtn = false;
-      this.isMoto = true;
+
+  get f() {
+    return this.form.controls;
+  }
+  
+  onSubmitCarForm() {
+    console.log(this.form);
+    const year = this.form.get('annee').value.getFullYear();
+    console.log(year);
+
+    if (this.form.valid) {
+      console.log(this.form.value);
     }
+  }
+  addCar() {
+    this.displayAddCar = true;
   }
 }
--- gestionGarage/src/app/app.module.ts
+++ gestionGarage/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {InputTextModule} from 'primeng/inputtext';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonModule} from 'primeng/button';
 import {RadioButtonModule} from 'primeng/radiobutton';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {TableModule} from 'primeng/table';
 import {ListboxModule} from 'primeng/listbox';
+import {DialogModule} from 'primeng/dialog';
+import {CalendarModule} from 'primeng/calendar';
+import {ColorPickerModule} from 'primeng/colorpicker';
+import {DropdownModule} from 'primeng/dropdown';
+import { CarService } from './services/carBrandsService.service';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -18,10 +26,18 @@ import {ListboxModule} from 'primeng/listbox';
     ButtonModule,
     RadioButtonModule,
     FormsModule,
+    ReactiveFormsModule,
     TableModule,
-    ListboxModule
+    ListboxModule,
+    DialogModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    CalendarModule,
+    ColorPickerModule,
+    DropdownModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
